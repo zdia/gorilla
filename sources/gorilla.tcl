5b3be9ba867fb4c74a3a40472931f6b4d849586b
Richard Ellis <github-email@dp100.com>
2019-02-23 14:45:32 -0500

    Update version number to 1.6.0-beta-1.

--- sources/gorilla.tcl
+++ sources/gorilla.tcl
@@ -32,7 +32,7 @@ package require Tcl 8.5-
 package provide app-gorilla 1.0
 
 namespace eval ::gorilla {
-	variable Version "1.5.3.8"
+	variable Version "1.6.0-beta-1"
 
 	# find the location of the install directory even when "executing" a symlink
 	# pointing to the gorilla.tcl file
